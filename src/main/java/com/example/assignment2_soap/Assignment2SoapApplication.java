b02bdf6853cc985de85ca80346fd4d8bf616256b
RNO321 <arno.burnuk@gmail.com>
2022-10-30 17:19:16 -0400

    init commit

---
+++ src/main/java/com/example/assignment2_soap/Assignment2SoapApplication.java
@@ -0,0 +1,16 @@
+package com.example.assignment2_soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Assignment2SoapApplication {
+	
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(Assignment2SoapApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/example/assignment2_soap/Assignment2SoapApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.assignment2_soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class Assignment2SoapApplicationTests {
+	
+	
+	@Test
+	void contextLoads() {
+	
+	}
+	
+}
